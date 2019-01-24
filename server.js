8159261e183ff2e4a822068837ca9854e1536b0b
JustTams <sarg.tier1@gmail.com>
2019-01-24 17:39:50 -0600

    second commit form sal

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// dependencies 
-const express = require('express');
-const mongoose =require('mongoose');
-const routes = ""
-
+// dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./app/routes/apiRoutes")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +9,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./app/routes/apiRoutes")(app)
+// serve up static assets for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// sets up routes for API and views
+app.use(routes)
+require("./app/routes/apiRoutes")(app);
+
+// connect to Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/{reactreadinglist}");
 
 app.listen(PORT);
