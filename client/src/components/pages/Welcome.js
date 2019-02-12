3d280ec207d5a30837ee879a700be1d6b8eb6bae
JustTams <sarg.tier1@gmail.com>
2019-02-12 17:21:46 -0600

    grammer update

--- client/src/components/pages/Welcome.js
+++ client/src/components/pages/Welcome.js
@@ -8,16 +8,16 @@ class Welcome extends React.Component {
           Welcome to Scion! It is our mission to help connect intended parents,
           and gestational carriers together in an attempt to assist those who
           are having difficulties bringing life into this world. Through our
-          application we make the difficult process of finding suitable
+          application we make the difficult, opaque process of finding suitable
           candidates not only more efficient, but transparent for both of the
           primary parties involved. We focus on the key attributes from each
           party that will ultimately facilitate this process, which, in turn
           will lead to more successful results from artificial insemination. Our
           app is currently in its infancy, but we are diligently working on
           rolling out a more complete experience that will further assist
-          individuals navigate the tremulous legal, psychological, and payment
-          regulations which govern the act of creating life through the act of
-          artificial insemination.
+          individuals navigate the tumultuous legal, psychological, and payment
+          regulations which govern the act of creating life through artificial
+          insemination.
         </p>
       </div>
     );
