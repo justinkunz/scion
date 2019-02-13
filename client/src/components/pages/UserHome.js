968afbfa38a0879b45d72118bb9f3c62ad15c820
Justin Kunz <Justin_Kunz@yahoo.com>
2019-02-12 23:43:13 -0600

    heroku push

--- client/src/components/pages/UserHome.js
+++ client/src/components/pages/UserHome.js
@@ -262,7 +262,6 @@ class UserHome extends Component {
                             className="text-center"
                             style={{
                               fontSize: "350%",
-                              fontFamily: "Baloo Bhaijaan, cursive",
                               transform: "translateY(-50%)",
                               position: "relative",
                               top: "50%",
